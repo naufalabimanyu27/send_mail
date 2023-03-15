831667485fc42849eb5f4d9aadc7dc0821914386
naufalabimanyu27 <opayabumanyu@gmail.com>
2023-03-15 15:06:48 +0700

    SAK

--- config/oracle.php
+++ config/oracle.php
@@ -3,16 +3,11 @@
 return [
     'oracle' => [
         'driver'         => 'oracle',
-        'tns'            => env('DB_TNS', ''),
-        'host'           => env('DB_HOST', ''),
-        'port'           => env('DB_PORT', '1521'),
-        'database'       => env('DB_DATABASE', ''),
-        'username'       => env('DB_USERNAME', ''),
-        'password'       => env('DB_PASSWORD', ''),
-        'charset'        => env('DB_CHARSET', 'AL32UTF8'),
-        'prefix'         => env('DB_PREFIX', ''),
-        'prefix_schema'  => env('DB_SCHEMA_PREFIX', ''),
-        'edition'        => env('DB_EDITION', 'ora$base'),
-        'server_version' => env('DB_SERVER_VERSION', '11g'),
+        'host'           => '203.153.96.250',
+        'port'           => '1521',
+        'database'       => 'ORION',
+        'username'       => 'ENDUSER',
+        'password'       => 'Nu5er123',
+        'service_name' => 'ORION',
     ],
 ];
